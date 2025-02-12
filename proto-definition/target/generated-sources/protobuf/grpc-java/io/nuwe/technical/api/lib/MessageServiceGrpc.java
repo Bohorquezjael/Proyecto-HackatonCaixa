3286767424ec1baace8b3294a2df4de05ea74148
Jael Bohorquez <Jaelbkz@icloud.com>
2025-02-12 01:07:40 -0600

    first commit

---
+++ proto-definition/target/generated-sources/protobuf/grpc-java/io/nuwe/technical/api/lib/MessageServiceGrpc.java
@@ -0,0 +1,237 @@
+package io.nuwe.technical.api.lib;
+
+import static io.grpc.MethodDescriptor.generateFullMethodName;
+
+/**
+ * <pre>
+ * Gets all the messages sent from (userSenderId) to (userReceiverId)
+ * </pre>
+ */
+@javax.annotation.Generated(
+    value = "by gRPC proto compiler (version 1.64.0)",
+    comments = "Source: message.proto")
+@io.grpc.stub.annotations.GrpcGenerated
+public final class MessageServiceGrpc {
+
+  private MessageServiceGrpc() {}
+
+  public static final java.lang.String SERVICE_NAME = "MessageProto.MessageService";
+
+  // Static method descriptors that strictly reflect the proto.
+  /**
+   * Creates a new async stub that supports all call types for the service
+   */
+  public static MessageServiceStub newStub(io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<MessageServiceStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<MessageServiceStub>() {
+        @java.lang.Override
+        public MessageServiceStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new MessageServiceStub(channel, callOptions);
+        }
+      };
+    return MessageServiceStub.newStub(factory, channel);
+  }
+
+  /**
+   * Creates a new blocking-style stub that supports unary and streaming output calls on the service
+   */
+  public static MessageServiceBlockingStub newBlockingStub(
+      io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<MessageServiceBlockingStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<MessageServiceBlockingStub>() {
+        @java.lang.Override
+        public MessageServiceBlockingStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new MessageServiceBlockingStub(channel, callOptions);
+        }
+      };
+    return MessageServiceBlockingStub.newStub(factory, channel);
+  }
+
+  /**
+   * Creates a new ListenableFuture-style stub that supports unary calls on the service
+   */
+  public static MessageServiceFutureStub newFutureStub(
+      io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<MessageServiceFutureStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<MessageServiceFutureStub>() {
+        @java.lang.Override
+        public MessageServiceFutureStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new MessageServiceFutureStub(channel, callOptions);
+        }
+      };
+    return MessageServiceFutureStub.newStub(factory, channel);
+  }
+
+  /**
+   * <pre>
+   * Gets all the messages sent from (userSenderId) to (userReceiverId)
+   * </pre>
+   */
+  public interface AsyncService {
+  }
+
+  /**
+   * Base class for the server implementation of the service MessageService.
+   * <pre>
+   * Gets all the messages sent from (userSenderId) to (userReceiverId)
+   * </pre>
+   */
+  public static abstract class MessageServiceImplBase
+      implements io.grpc.BindableService, AsyncService {
+
+    @java.lang.Override public final io.grpc.ServerServiceDefinition bindService() {
+      return MessageServiceGrpc.bindService(this);
+    }
+  }
+
+  /**
+   * A stub to allow clients to do asynchronous rpc calls to service MessageService.
+   * <pre>
+   * Gets all the messages sent from (userSenderId) to (userReceiverId)
+   * </pre>
+   */
+  public static final class MessageServiceStub
+      extends io.grpc.stub.AbstractAsyncStub<MessageServiceStub> {
+    private MessageServiceStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected MessageServiceStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new MessageServiceStub(channel, callOptions);
+    }
+  }
+
+  /**
+   * A stub to allow clients to do synchronous rpc calls to service MessageService.
+   * <pre>
+   * Gets all the messages sent from (userSenderId) to (userReceiverId)
+   * </pre>
+   */
+  public static final class MessageServiceBlockingStub
+      extends io.grpc.stub.AbstractBlockingStub<MessageServiceBlockingStub> {
+    private MessageServiceBlockingStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected MessageServiceBlockingStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new MessageServiceBlockingStub(channel, callOptions);
+    }
+  }
+
+  /**
+   * A stub to allow clients to do ListenableFuture-style rpc calls to service MessageService.
+   * <pre>
+   * Gets all the messages sent from (userSenderId) to (userReceiverId)
+   * </pre>
+   */
+  public static final class MessageServiceFutureStub
+      extends io.grpc.stub.AbstractFutureStub<MessageServiceFutureStub> {
+    private MessageServiceFutureStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected MessageServiceFutureStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new MessageServiceFutureStub(channel, callOptions);
+    }
+  }
+
+
+  private static final class MethodHandlers<Req, Resp> implements
+      io.grpc.stub.ServerCalls.UnaryMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
+    private final AsyncService serviceImpl;
+    private final int methodId;
+
+    MethodHandlers(AsyncService serviceImpl, int methodId) {
+      this.serviceImpl = serviceImpl;
+      this.methodId = methodId;
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public void invoke(Req request, io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        default:
+          throw new AssertionError();
+      }
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public io.grpc.stub.StreamObserver<Req> invoke(
+        io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        default:
+          throw new AssertionError();
+      }
+    }
+  }
+
+  public static final io.grpc.ServerServiceDefinition bindService(AsyncService service) {
+    return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
+        .build();
+  }
+
+  private static abstract class MessageServiceBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoFileDescriptorSupplier, io.grpc.protobuf.ProtoServiceDescriptorSupplier {
+    MessageServiceBaseDescriptorSupplier() {}
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.FileDescriptor getFileDescriptor() {
+      return io.nuwe.technical.api.lib.MessageProto.getDescriptor();
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.ServiceDescriptor getServiceDescriptor() {
+      return getFileDescriptor().findServiceByName("MessageService");
+    }
+  }
+
+  private static final class MessageServiceFileDescriptorSupplier
+      extends MessageServiceBaseDescriptorSupplier {
+    MessageServiceFileDescriptorSupplier() {}
+  }
+
+  private static final class MessageServiceMethodDescriptorSupplier
+      extends MessageServiceBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoMethodDescriptorSupplier {
+    private final java.lang.String methodName;
+
+    MessageServiceMethodDescriptorSupplier(java.lang.String methodName) {
+      this.methodName = methodName;
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.MethodDescriptor getMethodDescriptor() {
+      return getServiceDescriptor().findMethodByName(methodName);
+    }
+  }
+
+  private static volatile io.grpc.ServiceDescriptor serviceDescriptor;
+
+  public static io.grpc.ServiceDescriptor getServiceDescriptor() {
+    io.grpc.ServiceDescriptor result = serviceDescriptor;
+    if (result == null) {
+      synchronized (MessageServiceGrpc.class) {
+        result = serviceDescriptor;
+        if (result == null) {
+          serviceDescriptor = result = io.grpc.ServiceDescriptor.newBuilder(SERVICE_NAME)
+              .setSchemaDescriptor(new MessageServiceFileDescriptorSupplier())
+              .build();
+        }
+      }
+    }
+    return result;
+  }
+}
